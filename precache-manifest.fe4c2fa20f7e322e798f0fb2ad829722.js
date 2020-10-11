b004354f9a082eb022a32c33f9020fb35ab9e0e3
eikcalb <agwaisraelonome@gmail.com>
2020-10-12 00:21:53 +0100

    Updates

---
+++ precache-manifest.fe4c2fa20f7e322e798f0fb2ad829722.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f5524c566ce801cba772ee84c3b27b9",
+    "url": "/contractoradmin/index.html"
+  },
+  {
+    "revision": "854d3f4ce9feb17740ff",
+    "url": "/contractoradmin/static/css/2.240f97b7.chunk.css"
+  },
+  {
+    "revision": "e570b2e4547bec25cb3c",
+    "url": "/contractoradmin/static/css/main.83f5d92a.chunk.css"
+  },
+  {
+    "revision": "854d3f4ce9feb17740ff",
+    "url": "/contractoradmin/static/js/2.7dc1e4bf.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/contractoradmin/static/js/2.7dc1e4bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e570b2e4547bec25cb3c",
+    "url": "/contractoradmin/static/js/main.57eaa1d6.chunk.js"
+  },
+  {
+    "revision": "ff975ed21cdca5f9e7e8",
+    "url": "/contractoradmin/static/js/runtime-main.dfb63559.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/contractoradmin/static/media/logo.5d5d9eef.svg"
+  }
+]);
